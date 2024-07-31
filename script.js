1fb04738bff275b7410b565fc219148603c8a26c
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-31 17:35:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+player.addEventListner("click",()=>{
+	if(video.paused){
+		video.play();
+	}else{
+		video.pause();
+	}
+})
+
+video.addEventListener('play',()=>{
+	toggle.textContent = '❚❚';
+});
+video.addEventListener('pause',()=>{
+	toggle.textContent ='►';
+});
+
+video.addEventListner('timeupdate',()=>{
+	const progress = (video.currentTime/video.duration)*100;
+	progressBar.style.flexBasis = ${progress}%;
+});
+
+ranges[1].addEventListener('input',()=>{
+	video.playbackRate = ranges[1].value;
+});
+
+skipButtons.forEach(button =>{
+	button.addEventListener('click',()=>{
+		video.currentTime += parseFloat(button.dataset.skip)
+	});
+});
